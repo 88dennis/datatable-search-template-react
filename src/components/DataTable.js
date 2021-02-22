9655ca55a8b45b12b359946957d796930f4f79f8
Dennis Sarmiento <dennissarmiento8080@gmail.com>
2021-02-21 19:48:07 -0500

    up

---
+++ src/components/DataTable.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+
+// let isMounted = false;
+const DataTable = ({data}) => {
+console.log(data)
+const columns = data[0] && Object.keys(data[0])
+    return (
+        <>
+            {/* <table className="table" cellPadding={0} cellSpacing={0}> */}
+          
+            <table className="table">
+                <thead>
+                    <tr>
+                        {data[0] && columns.map((heading, index) => <th key={index}scope="col">{heading}</th>)}
+                    </tr>   
+                </thead>
+                <tbody>
+                    {data.map((row, index)=> <tr key={index}>
+                        {
+                            // columns.map(column => <td>{JSON.stringify(row[column])}</td>)
+                            columns.map((column, index) => <td key={index}>{row[column]}</td>)
+
+                            }
+                        </tr>)}
+                </tbody>
+            </table>
+
+        </>
+    )
+}
+
+export default DataTable;
---
+++ src/components/Home.js
@@ -0,0 +1,108 @@
+import React, { useState, useEffect } from "react";
+import DataTable from "./DataTable";
+
+// let isMounted = false;
+const Home = () => {
+  const [data, setData] = useState([]);
+  const [searchColumns, setSearchColumns] = useState(["borrowerFirstName", "borrowerUserName"]);
+
+
+  const [query, setQuery] = useState("");
+
+  const fetchData = async () => {
+    return await fetch("/data.json")
+      .then((response) => response.json())
+      .then((json) => {
+        setData(json);
+      });
+  };
+  useEffect(() => {
+    fetchData();
+  }, [query, searchColumns]);
+
+
+//   function search(itemsFromData){
+//       return itemsFromData.filter(dataItem => {
+          
+//         return dataItem.borrowerFirstName.toLowerCase().indexOf(query) > -1 ||
+//         dataItem.borrowerUserName.toLowerCase().indexOf(query) > -1 ||
+//         dataItem.borrowerEmail.toLowerCase().indexOf(query) > -1 ||
+//         dataItem.lenderUserName.toLowerCase().indexOf(query) > -1 ||
+//         dataItem.lenderEmail.toLowerCase().indexOf(query) > -1
+    
+//     }
+      
+//       )
+//   }
+
+//REFACTOR - FUNCTION TO SEARCH ALL COLUMNS IN YOUR TABLE BASICALLY SEARCH ALL YOUR DATA
+//   function search(itemsFromData){
+//       const columns = itemsFromData[0] && Object.keys(itemsFromData[0])
+//       return itemsFromData.filter(dataItem => 
+//          columns.some(
+//             (column) => dataItem[column].toString().toLowerCase().indexOf(query.toLowerCase()) > -1
+//             )
+//       )
+//   }
+
+//TO SEARCH ONLY SPECIFIC COLUMNS
+  function search(itemsFromData){
+    return itemsFromData.filter(dataItem => 
+       searchColumns.some(
+          (column) => dataItem[column].toString().toLowerCase().indexOf(query.toLowerCase()) > -1
+          )
+    )
+}
+
+const columns = data[0] && Object.keys(data[0]);
+console.log(columns)
+
+
+  return (
+    <>
+      <h1>Filter Goes Here</h1>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6 offset-md-3 col-sm-12">
+            <form>
+              <div className="form-group">
+                <label htmlFor="queryInputId">Search</label>
+                <input
+                  type="text"
+                  value={query || ''}
+                  onChange={(e)=> setQuery(e.target.value)}
+                  className="form-control"
+                  id="queryInputId"
+                  placeholder="Search"
+                />
+              </div>
+       
+            </form>
+          </div>
+        </div>
+        <div className="row">
+           <div className="col-md-6 col-md-6 offset-md-3 col-sm-12">
+
+               {columns && columns.map((column)=> {
+                   return <> <label>
+                       <input type="checkbox" checked={searchColumns.includes(column)}
+                       onChange={(e)=>{
+                           const checked = searchColumns.includes(column);
+                           setSearchColumns(
+                            (prev) => checked ? prev.filter((sc)=> sc !== column) : [...prev, column]
+                            );
+                       }}
+                       />
+                       <span>{" "}</span> <span>{column}</span>
+
+                   </label><span>{" "}</span>
+                   </> })}
+           </div>
+        </div>
+      </div>
+      <DataTable data={search(data)} />
+    </>
+  );
+};
+
+export default Home;
