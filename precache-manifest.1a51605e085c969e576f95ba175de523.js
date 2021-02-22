39d162e070cb34bfe24fa8a7a780ba0623f2c711
Dennis Sarmiento <dennissarmiento8080@gmail.com>
2021-02-21 20:41:22 -0500

    Updates

---
+++ precache-manifest.1a51605e085c969e576f95ba175de523.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5bd28229e50b36053b17b31fdb04a1d",
+    "url": "/datatable-search-template-react/index.html"
+  },
+  {
+    "revision": "844ff62e3b44eb9fe4c0",
+    "url": "/datatable-search-template-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2726497c56aa778ac3e8",
+    "url": "/datatable-search-template-react/static/js/2.e7b85426.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/datatable-search-template-react/static/js/2.e7b85426.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "844ff62e3b44eb9fe4c0",
+    "url": "/datatable-search-template-react/static/js/main.c468895a.chunk.js"
+  },
+  {
+    "revision": "777e1ea9e7ac608d3410",
+    "url": "/datatable-search-template-react/static/js/runtime-main.2e72f6a9.js"
+  }
+]);
